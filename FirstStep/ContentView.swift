4d24580abfb4795fea1cd6751f32e5010fe89103
Mighty Burapachaisri <saranb23@rism.ac.th>
2021-08-05 21:06:33 +0700

    Initial Commit

---
+++ FirstStep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstStep
+//
+//  Created by Mighty  on 5/8/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstStep/FirstStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstStepApp.swift
+//  FirstStep
+//
+//  Created by Mighty  on 5/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct FirstStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
