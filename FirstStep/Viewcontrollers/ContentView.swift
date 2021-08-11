6ee01debcd39eeb166515389dee7d0c4e1bda219
MightyB11 <saranb23@rism.ac.th>
2021-08-11 13:19:32 +0700

    11/8/21: Adding Side Menu to home page (Not done yet)

--- FirstStep/Viewcontrollers/ContentView.swift
+++ FirstStep/Viewcontrollers/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     private var healthStore: HealthStore?
     @State private var steps: [Step] = [Step]()
     
+    
     init() {
         healthStore = HealthStore()
     }
@@ -32,8 +33,8 @@ struct ContentView: View {
         }
         
     }
+    
     var body: some View {
-        
         ZStack {
             Color(#colorLiteral(red: 0.8640606999, green: 0.9640640616, blue: 0.949200213, alpha: 1))
                 .ignoresSafeArea()
@@ -43,7 +44,9 @@ struct ContentView: View {
                 stepCounter()
                 attractions1()
                 }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             }
+            
     
         .onAppear {
             if let healthStore = healthStore {
@@ -69,20 +72,34 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+
 struct topMenu: View {
+    @State var menuOpened = false
     var body: some View {
         HStack(){
             Spacer()
-            Button(action: {
-                
-            }, label: {
-                Image("Menu")
-                    .resizable()
-                    .frame(width:70, height:70)
-                    .padding(.trailing, 80.0)
-                    .padding(.leading, 30.0)
-            })
-            Spacer()
+            ZStack{
+                if !menuOpened{
+                    Button(action: {
+                        //open menu
+                        self.menuOpened.toggle()
+                    }, label: {
+                        Image("Menu")
+                            .resizable()
+                            .frame(width:70, height:70)
+                            .padding(.trailing, 80.0)
+                            .padding(.leading, 30.0)
+                    })
+               
+                }
+                sideMenu(width: UIScreen.main.bounds.width/1.8,
+                         menuOpened: menuOpened,
+                         toggleMenu: toggleMenu)
+
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            
             Button(action: {
                 
             }, label: {
@@ -93,13 +110,98 @@ struct topMenu: View {
                     .padding(.trailing, 30.0)
             })
             Spacer()
+                }
+        
+        
         }
-        .padding(.top, 40.0)
-    
+    func toggleMenu(){
+        menuOpened.toggle()
+        
     }
 }
-    
 
+
+struct menuItem: Identifiable{
+    var id = UUID ()
+    let text: String
+    let imageName: String
+    let handler: () -> Void = {
+        print("Tapped Item")
+    }
+}
+struct menuContent: View {
+    let items: [menuItem] = [
+        menuItem(text: "Home", imageName: "custom.flame.fill"),
+        menuItem(text: "Journey", imageName: "figure.walk"),
+        menuItem(text: "Leaderboard", imageName: "list.starl"),
+        menuItem(text: "Achievements", imageName: "achievements"),
+        menuItem(text: "Settings", imageName: "custom.gear"),
+    
+    ]
+    
+    var body: some View {
+       
+        ZStack {
+            Color(UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1))
+        }
+        
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach(items) { item in
+                HStack{
+                    Image(systemName: item.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode:.fit)
+                        .frame(width: 22, height: 22, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text(item.text)
+                        .bold()
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 20))
+                    
+                    Spacer()
+                }
+                .onTapGesture {
+                    
+                }
+                .padding()
+                Divider()
+                
+            }
+            Spacer()
+        }
+        .padding(.top, 25)
+    }
+}
+
+struct sideMenu: View {
+    let width: CGFloat
+    let menuOpened: Bool
+    let toggleMenu: () -> Void
+   
+    var body: some View {
+        ZStack{
+            //Dimmed background view
+            GeometryReader{ _ in
+                EmptyView()
+            }
+            .background(Color.green.opacity(0.25))
+            .opacity(self.menuOpened ? 1:0)
+            .animation(Animation.easeIn.delay(0.5))
+            .onTapGesture {
+                self.toggleMenu()
+            }
+            
+            //menuContent
+            HStack{
+                menuContent()
+                    .frame(width: width)
+                    .offset(x: menuOpened ? 0: -width)
+                    .animation(.default)
+                    
+                Spacer()
+            }
+        }
+    }
+}
 struct stepCounter: View {
     var body: some View {
         VStack(spacing:-20){
@@ -132,7 +234,6 @@ struct stepCounter: View {
         .padding(.top, 100.0).padding(.horizontal, 15)
     }
 }
-
 
 struct attractions1: View {
     var body: some View {
